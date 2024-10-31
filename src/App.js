446e0b445acd058390344dec6560746a8f87b7a9
ssubhan21 <mohdabdulsubhan3@gmail.com>
2024-10-31 18:42:23 -0400

    title changed

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ const App = () => {
   return (
     <div className="App">
       <h1>Student Information System</h1>
-      <h5>Developed By: 101397394 || Subhan Mohammed Abdul || DevOps</h5>
+      <h5>Developed By: 101397394 || Subhan Mohammed Abdul || COMP3104</h5>
       <StudentForm addStudent={addStudent} />
       <StudentList students={students} />
     </div>
